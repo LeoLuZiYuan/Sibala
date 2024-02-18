0256b89189de706b2c857ef3a0cf6407318f234c
梓元 盧 <asd88551144@gmail.com>
2024-02-18 10:56:00 +0800

    [refactor to logic] use player2 name and dic output for winner  output

--- Sibala/Game.cs
+++ Sibala/Game.cs
@@ -5,6 +5,13 @@ public class Game
     public string Result(string input)
     {
         // Black:5 3 5 4  White:2 6 2 3
-        return "White win. - with normal point: 6 over 3";
+        var players = new Parser().Parse(input);
+
+        var winnerPlayer = players[1].Name;
+        var groupBy = players[1].Dices.OrderByDescending(x => x.Value).ToList()
+            .GroupBy(x => x.Value).ToList();
+        var winnerOutput = $"{groupBy[0].First().Output} over {groupBy[1].First().Output}";
+
+        return $"{winnerPlayer} win. - with normal point: {winnerOutput}";
     }
 }
